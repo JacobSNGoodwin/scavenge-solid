bd816d3596ef91f58c2ccf2f1024e4093fbbaf4b
Jacob Goodwin <jacob.goodwin@gmail.com>
2024-01-19 16:19:51 -0800

    add uno css to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,20 @@
-import { defineConfig } from "@solidjs/start/config";
+import { defineConfig } from '@solidjs/start/config';
+import UnoCSS from 'unocss/vite';
+import { presetIcons, presetWebFonts, presetUno } from 'unocss';
 
-export default defineConfig({});
+export default defineConfig({
+	plugins: [
+		UnoCSS({
+			presets: [
+				presetUno(),
+				presetIcons(),
+				presetWebFonts({
+					provider: 'none',
+					fonts: {
+						sans: 'Lato',
+					},
+				}),
+			],
+		}),
+	],
+});
